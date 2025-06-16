3326ee32fd9fc7ef67e131573e0013987859f74f
Mathieu <mathieu.mjr@gmail.com>
2025-06-16 14:13:44 +0000

    added description in betty style

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-/* Main is the entry point of my program*/
+/**
+ * main - Is the entry point
+ * of my program
+ * @void
+(*
+ * Description: Compilator will look for
+ * it to start)?
+(* section header: will call stdio library)*
+ * Return: if sucess, will return 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
