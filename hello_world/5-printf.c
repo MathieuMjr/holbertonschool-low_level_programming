46c6015ac109931e15c32d35df4aaa722326c69a
Mathieu <mathieu.mjr@gmail.com>
2025-06-16 14:25:32 +0000

    script that print with printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <shio.h>
+
+/**
+ * main - Is the entry point
+ * of my program
+ * @void
+(*
+ * Description: Compilator will look for
+ * it to start)?
+(* section header: will call stdio library)*
+ * Return: if sucess, will return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
