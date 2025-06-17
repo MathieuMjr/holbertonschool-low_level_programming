ded83878a196b2d9f8ac0f90c58ce9331660c4f8
Mathieu <mathieu.mjr@gmail.com>
2025-06-17 15:01:05 +0000

    print numberz but with putcar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Is the entry point
+ * of my program
+ * @void
+(*
+ * Description: Compilator will look for
+ * it to start)?
+(* section header: will call stdio library)*
+ * Return: if sucess, will return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; ++i)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
