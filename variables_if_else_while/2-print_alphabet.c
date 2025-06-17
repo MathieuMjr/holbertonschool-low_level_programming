bf9addb821bdccb9a13cc7a843708238c5cef66d
Mathieu <mathieu.mjr@gmail.com>
2025-06-17 13:36:27 +0000

    script that print the alphabet ; hope it will make Betty happy

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Is the entry point
+ * of my program
+ * @void
+(*
+ * Description: Compilator will look for
+ * it to start)?
+(* section header: will call stdio library)*
+ * Return: if sucess, will return 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 97; c <= 121; ++c)
+	{
+		printf("%c", c);
+	}
+	printf("%c\n", c);
+	return (0);
+}
