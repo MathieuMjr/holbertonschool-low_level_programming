7f5f1d199064041f7cdc55a24a2c8d7e084d844c
Mathieu <mathieu.mjr@gmail.com>
2025-06-17 14:52:42 +0000

    script that print numbers from 0 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Is the entry point
+ * of my program
+ * @void
+(*
+ * Description: Compilator will look for
+ * it to start)?
+(* section header: will call stdio library)*
+ * Return: if sucess, will return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
