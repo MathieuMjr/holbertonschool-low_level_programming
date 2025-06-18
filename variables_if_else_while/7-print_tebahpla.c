3a6b93a8024e4ac2e7aba19a95fcb0ba9126d2e9
Mathieu <mathieu.mjr@gmail.com>
2025-06-18 08:01:15 +0000

    script that print reverse alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Is the entry point
+ * of my program
+ * @void
+(*
+ * Description: Compilator will look for
+ * it to start)?
+(* section header: will call stdio library)*
+ * Return: if sucess, will return 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; --c)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
