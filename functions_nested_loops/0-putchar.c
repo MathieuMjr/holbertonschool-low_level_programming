8f3229374102a83084921ec0dfa941c5b79effeb
Mathieu <mathieu.mjr@gmail.com>
2025-06-19 09:51:17 +0000

    script that print _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <main.h>
+
+/**
+ * main - Is the entry point
+ * of my program
+ * @void
+(*
+ * Description: Compilator will look for
+ * it to start)?
+(* section header: will call stdio library)*
+ * Return: if sucess, will return 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
