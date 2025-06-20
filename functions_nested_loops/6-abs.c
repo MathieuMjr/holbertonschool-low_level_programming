ed4b0bf8e279c2c9875822dcad6a7062d1cd4ae6
Mathieu <mathieu.mjr@gmail.com>
2025-06-20 09:55:23 +0000

    code that gives the absolute value of an int

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - gives absolute value of an int
+ * @int : takes an int value
+ (*
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: absolute value
+ */
+int _abs(int i)
+{
+
+	if (i < 0)
+	{
+		return (i * -1);
+	}
+	else
+	{
+		return (i);
+	}
+}
