13baa06d07e586cc1f0441dc0ab02e690c85b6b0
Mathieu <mathieu.mjr@gmail.com>
2025-06-20 22:54:36 +0200

    code to make an add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - makes an add with 2 ints
+ * @a : one number you want to add to another
+ * @b : another number tyou want to add to the 1st one
+ (*
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: result of operation
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
