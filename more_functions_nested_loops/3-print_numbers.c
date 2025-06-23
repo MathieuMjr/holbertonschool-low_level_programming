27dadf7cca20f277396d197f7ac996435bd6baeb
Mathieu <mathieu.mjr@gmail.com>
2025-06-23 10:23:45 +0200

    code that print digit from 0 to 9 with _putchar twice and no more

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ (*
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; ++i)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
