445bb67d702b4a5783b77475349a04fe694503d7
Mathieu <mathieu.mjr@gmail.com>
2025-06-23 10:57:21 +0200

    code that print numbers from 0 to 14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers from 0 to 14
+ (*
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 14; ++i)
+	{
+		if (i <= 9)
+		{
+			_putchar('0' + i);
+		}
+		else
+		{
+			_putchar('0' + (i / 10));
+			_putchar('0' + (i % 10));
+		}
+	}
+	_putchar('\n');
+}
