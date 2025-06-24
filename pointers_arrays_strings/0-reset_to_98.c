55589b3e30ed5c482d404cd1afd963c74464292f
Mathieu <mathieu.mjr@gmail.com>
2025-06-24 11:06:45 +0200

    Code that change value of a var through its pointer

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - change de value of a var
+ * @n: pointer to aadress of the var
+ (*
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
