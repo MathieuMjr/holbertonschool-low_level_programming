464178901993f6133439dfa852cc595baa0c9430
Mathieu <mathieu.mjr@gmail.com>
2025-06-25 16:40:57 +0200

    try a code that copy a string from a buffer to a destination array

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - takes a source and put it in an array
+ * @dest: array of destination
+ * @src: string to use as source to put in the array
+ (*
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: char string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' ; ++i)
+	{
+		dest[i] = src[i];
+	}
+	dest[i + 1] = src[i + 1];
+	return (dest);
+}
