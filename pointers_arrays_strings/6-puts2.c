cbbfedbf04c437b8a268e42fefaf085c808e1b16
Mathieu <mathieu.mjr@gmail.com>
2025-06-25 13:46:25 +0200

    function that print even number from a string of char

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts2 - print numbers of a string if it's an even number
+ * @str: adress of a char var
+ (*
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0; /*compteur pour while : donnera la longueur de la string*/
+	int j; /*compteur pour for */
+
+	while (str[i] != '\0')
+	{
+		++i;
+	}
+	for (j = 0; j <= i; ++j)
+	{
+		if (str[j] % 2 == 0)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
+}
