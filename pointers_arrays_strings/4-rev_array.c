20cb5ca01f2f1b89d60e7897f8fa5c4139d69244
Mathieu <mathieu.mjr@gmail.com>
2025-06-27 13:49:07 +0200

    function that reverse and array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverse content of an array
+ * @*a: Pointer to the destination of an int array.
+ * @n: size of this array.
+ (*
+ * Description: if string 1 is greater than string 2, return a positive
+ * number, 0 is they have the same lenght and a negative number otherwise
+(* section header: Contains)*
+ * Return: a positive number if greater, a negative if less, 0 if equal
+ */
+
+void reverse_array(int *a, int n)
+{	
+	int j;
+	int c;
+
+		for (j = 0; j < (n / 2); ++j)
+		{
+			c = a[j];
+			a[j] = a[n - j - 1];
+			a[n - j - 1] = c;
+		}
+	}
+
