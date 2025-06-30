4313137b9b55d12d6f17fc4451cae4791b7a8e10
Mathieu <mathieu.mjr@gmail.com>
2025-06-30 16:13:13 +0200

    function in progress

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searchs of char in a string ; as soon as one is found,
+ * it prints every other chars of the string
+ * @s: Pointer to the string to explore.
+ * @accept: Pointer to a list of char we are looking for.
+ (*
+ * Description: 
+(* section header: Contains)*
+ * Return: Return bytes in s that matches one of the bytes in accept
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int j = 0;
+	int k;
+	int b = 0;
+	
+	while (s[j] != 0)
+	{
+		for (k = 0; accept[k] != '\0' ; ++k)
+		{
+			if (s[j] == accept [k])
+			{
+				b++;
+				break;
+			}
+		}
+		if (b != 0)
+		{
+			break;
+		}
+		else
+		{
+			++j;
+		}
+	}
+	return (NULL);
+}
