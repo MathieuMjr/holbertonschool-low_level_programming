94f3ed2611186e4375f5f9ffc312d714d861f574
Mathieu <mathieu.mjr@gmail.com>
2025-06-30 14:26:08 +0200

    function that gets lenght of a prefix substring

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strpn - Returns the number of byte in a string that match specific 
+ * chars
+ * @s: Pointer to a source string
+ * @accept: Pointer to a list of char we are looking for in source.
+ (*
+ * Description: If a char from "accept" is found in "s", then count 
+ * how much space it takes. It stops as soon a char from accept is not found.
+(* section header: Contains)*
+ * Return: The number of bytes matching our "accept" list
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i =0;
+	unsigned int j = 0;
+	unsigned int k;
+	unsigned int b = 0 ; /* indicateur de présence d'un char recherché*/
+	
+	while (accept[j] != '\0')
+	{
+		++j;
+	}
+	while (s[i] != '\0')
+	{
+		for (k = 0; k <= (j - 1); ++k)
+		{
+			if (s[i] == accept[k])
+			{
+				++b;
+			}
+		}
+		if (b != (i + 1))
+		{
+			break;
+		}
+		++i;
+	}
+return (b);
+}
