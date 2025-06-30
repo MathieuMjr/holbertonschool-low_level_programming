b1021f4501574e240eb12f25b8020b1e115b03f3
Mathieu <mathieu.mjr@gmail.com>
2025-06-30 20:44:58 +0200

    function that finds substrings in  string

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -2,32 +2,50 @@
 
 /**
  * _strstr - Locate a substring in a string
- * @s1: Pointer to the source string.
- * @s2: Pointer to the substring qe are looking for.
+ * @haystack: Pointer to the source string.
+ * @needle: Pointer to the substring qe are looking for.
  (*
  * Description: if string 1 is greater than string 2, return a positive
  * number, 0 is they have the same lenght and a negative number otherwise
 (* section header: Contains)*
- * Return: the substring. 
+ * Return: the substring.
  */
 char *_strstr(char *haystack, char *needle)
 {
 	int i = 0;
-	int j = 0;
-	int k;
+	int j;
+	int k = 0;
+	int l = 0;
 
-	while (needle[j] != '\0')
+	while (needle[k] != '\0')
 	{
-		++j;
+		k++;
 	}
-	while (haystack[i] != '\0')
+	while (haystack[l] != '\0')
 	{
-
-	for (k = 0; (i - k) <= j; ++k)
+		++l;
+	}
+	printf("valeur de k: %d\n", k);
+	while (i < (l - k))
 	{
-		if (haystack[i] == needle[k])
+		if ((haystack[i]) == (needle[0]))
 		{
-			
-	}
+			for (j = 1; needle[j] != '\0'; ++j)
+			{
+				if (haystack[i + j] != needle[0 + j])
+				{
+					break;
+				}
+			}
+			if (j == k)
+			{
+				return (haystack + i);
+			}
+		}
+		else
+		{
 			++i;
+		}
+	}
+	return (NULL);
 }
