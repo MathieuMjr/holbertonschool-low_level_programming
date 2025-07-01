a0153ec0f6896d8891b2822225542671453270e7
Mathieu <mathieu.mjr@gmail.com>
2025-07-01 15:52:07 +0200

    function that print recursively backward

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - put a string backward 
+ * @s: point the adress of a string
+ (*
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+			_putchar(*s);
+	}
+}
