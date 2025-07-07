0c42f56406e6c6a6335192506b5d14ec940c3a28
Mathieu <mathieu.mjr@gmail.com>
2025-07-07 11:32:26 +0200

    fixed function to return NULL if malloc fails or size == 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,7 +14,17 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
 	array = malloc(size * sizeof(char));
+	if (array == NULL) /* toujours vérifier si j'ai eu assez */
+						/* mémoire dispo pour que malloc fonctionne */
+	{
+		return (NULL);
+	}
 	for (i = 0; i <= size; ++i)
 	{
 		array[i] = c;
