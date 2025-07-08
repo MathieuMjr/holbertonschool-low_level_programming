e3b03302ee240108523a1905a167ada06ab5b1be
Mathieu <mathieu.mjr@gmail.com>
2025-07-08 13:48:37 +0200

    commented

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -9,12 +9,22 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	 void *memalloc;
+	/* Attention void * ne signifie pas que la fonction retourne void
+	Mais qu'elle retourne un pointeur générique
+	Il faut bien retourner ! (un pointeur générique)*/
+	/* Déclaration d'un pointeur pour stocker l'adresse mémoire allouée */
+	void *memalloc;
 
+	/* Tentative d'allocation de b octets avec malloc */
 	memalloc = malloc(b);
+
+	/* Si malloc échoue, le pointeur sera NULL */
 	if (memalloc == NULL)
 	{
+		/* Quitte le programme avec un code d'erreur 98 */
 		exit(98);
 	}
+
+	/* Retourne le pointeur vers la mémoire allouée */
 	return (memalloc);
 }
