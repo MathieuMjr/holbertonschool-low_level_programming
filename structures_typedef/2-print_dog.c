ba3e8bf48ed85e0bd68349fb7d082ab488cf7726
Mathieu <mathieu.mjr@gmail.com>
2025-07-09 17:04:24 +0200

    fixed printf for age member

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -17,14 +17,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", (*d).name);
 	}
-	if ((*d).age == 0)
-	{
-		printf("Age: %d\n", 0);
-	}
-	else
-	{
-		printf("Age: %f\n", (*d).age);
-	}
+	printf("Age: %f\n", (*d).age);
 	if ((*d).owner == NULL)
 	{
 		printf("Owner: %p\n", NULL);
