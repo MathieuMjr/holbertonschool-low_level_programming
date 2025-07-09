5d38f2d6b266be622c53b19b1b8999c359ccabc4
Mathieu <mathieu.mjr@gmail.com>
2025-07-09 17:00:46 +0200

    function that print members of  a struct dog variable

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print members of a struct dog variable
+ * @d: adress of the variable
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if ((*d).name == NULL)
+	{
+		printf("Name :%p\n", NULL);
+	}
+	else
+	{
+		printf("Name: %s\n", (*d).name);
+	}
+	if ((*d).age == 0)
+	{
+		printf("Age :%p\n", NULL);
+	}
+	else
+	{
+		printf("Age: %f\n", (*d).age);
+	}
+	if ((*d).owner == NULL)
+	{
+		printf("Owner :%p\n", NULL);
+	}
+	else
+	{
+		printf("Owner: %s\n", (*d).owner);
+	}
+}
