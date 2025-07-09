4cb30cace557799a440cdbecaafdeac209357102
Mathieu <mathieu.mjr@gmail.com>
2025-07-09 15:05:33 +0200

    betty coding style

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - details of dogs
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
