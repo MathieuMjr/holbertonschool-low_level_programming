75cc89b82a70e07714c71832752a1918fa3cb16b
Mathieu <mathieu.mjr@gmail.com>
2025-07-09 16:19:09 +0200

    fixed where d is null

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "dog.h"
 
 /**
@@ -9,6 +10,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		return;
+	}
+
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
