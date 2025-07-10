53ca0b1ec32f0e5482cd962478480a06a7ee2715
Mathieu <mathieu.mjr@gmail.com>
2025-07-10 14:34:26 +0200

    function that create a new var of struct dog with typedef dog_t

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog- fonction that create a new variable of struct dog
+ * @name: pointeur to a name
+ * @age: age of the dog
+ * @owner: name of the dog's owner
+ * return : a pointeur a struct type dog_t
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *pouet; 
+
+	pouet = malloc(sizeof(dog_t));
+	if (pouet == NULL)
+	{
+		return (NULL);
+	}
+	pouet->name = name;
+	pouet->age = age;
+	pouet->owner = owner;
+
+	return(pouet);
+}
