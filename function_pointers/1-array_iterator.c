55310d4a7a4c2cbe7b9a03a5f95fcd255016bf9b
Mathieu <mathieu.mjr@gmail.com>
2025-07-14 15:27:09 +0200

    function that print an array in integer or hexadecimal mode

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - print numbers of an array
+ * @array: array of numbers to print
+ * @size: size of the array
+ * @action: function to use to print
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long int i;
+
+	for (i = 0; i <= size; ++i)
+	{
+		action(array[i]);
+	}
+}
