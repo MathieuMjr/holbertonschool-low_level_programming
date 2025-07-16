2696d95ed457f7e1b9e375c5b68add730d3e3375
Mathieu <mathieu.mjr@gmail.com>
2025-07-16 11:46:10 +0200

    commentaires

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -10,25 +10,38 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
-	unsigned int i;
-	const char *str;
+	va_list args; /* nommer la lister*/
+	unsigned int i; /* création du compteur pour parcourir la lsite*/
+	const char *str; /* pointeur qui stocke l'adresse des arguments 
+					voir plus loin pourquoi)*/
 
-	va_start(args, n);
-	if (separator == NULL)
+	va_start(args, n); /* on initialise la liste*/
+	if (separator == NULL) /* si rien n'est donné en arguments
+							on considère que c'est une chaine vide*/
 	{
 		separator = "";
 	}
 	for (i = 0; i < n; ++i)
 	{
-		str = va_arg(args, char*);
-		if (str == NULL)
+		str = va_arg(args, char*); 
+	/* si on appelle va_arg deux fois 
+	1 fois pour savoir si l'argument est NULL
+	une autre fois pour l'imprimer s'il ne l'est pas
+	Va_arg aura progressé à chaque appel dans la liste !
+	
+	On le stocke donc dans un pointeur. 
+	Ce pointeur doit toujours garder la même adresse, 
+	mais sa valeur doit changer. C'est pour ça qu'on indique
+	qu'il est constant lors de la déclaration de str
+	en le mettant avant le type*/
+		if (str == NULL) /* si NULL se trouve à l'adresse de str
+						à cette itération de va_arg*/
 		{
-			printf("%s", "(nil)");
+			printf("%s", "(nil)"); /* imprime nul*/
 		}
 		else
 		{
-			printf("%s", str);
+			printf("%s", str);/* sinon, imprime ce qui est pointé par str*/
 		}
 		if (i < n - 1)
 		{
