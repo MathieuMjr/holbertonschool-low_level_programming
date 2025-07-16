96ece1c5de25507ed0c30c19ca826755802f19d9
Mathieu <mathieu.mjr@gmail.com>
2025-07-16 11:41:58 +0200

    fixed case where va_arg is null without calling it twice !

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -12,6 +12,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
+	const char *str;
 
 	va_start(args, n);
 	if (separator == NULL)
@@ -20,11 +21,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	}
 	for (i = 0; i < n; ++i)
 	{
-		/*if (va_arg(args, char*) == NULL)
+		str = va_arg(args, char*);
+		if (str == NULL)
 		{
 			printf("%s", "(nil)");
-		}*/
-			printf("%s", va_arg(args, char*));
+		}
+		else
+		{
+			printf("%s", str);
+		}
 		if (i < n - 1)
 		{
 			printf("%s", separator);
