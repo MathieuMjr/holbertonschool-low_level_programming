ec2f421ddcad5a5d208025ef889066fcaa73101f
Mathieu <mathieu.mjr@gmail.com>
2025-07-17 14:43:19 +0200

    1st coding style fixed

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,20 +1,52 @@
 #include "variadic_functions.h"
 
+/**
+ * type_char - function that print an undefined
+ * arg that is a char
+ * @args: arg to print
+ *
+ * Return:
+ */
 void type_char(va_list args)
 {
 	printf("%c", va_arg(args, int));
 }
+
+/**
+ * type_int - function that print an undefined
+ * arg that is an int
+ * @args: arg to print
+ *
+ * Return:
+ */
 void type_int(va_list args)
 {
 	printf("%d", va_arg(args, int));
 }
+
+/**
+ * type_float - function that print an undefined
+ * arg that is a float
+ * @args: arg to print
+ *
+ * Return:
+ */
 void type_float(va_list args)
 {
 	printf("%f", va_arg(args, double));
 }
+
+/**
+ * type_string - function that print an undefined
+ * arg that is a string
+ * @args: arg to print
+ *
+ * Return:
+ */
 void type_string(va_list args)
 {
 	const char *s = va_arg(args, char*);
+
 	if (s == NULL)
 	{
 		printf("(nil)");
@@ -23,32 +55,26 @@ void type_string(va_list args)
 	printf("%s", s);
 }
 
-/*void (*get_data_type(char *s))
-{
-	int i = 0;
-
-	struct type type[] =
-	{
-		{"c", type_char},
-		{"i", type_int},
-		{"f", type_float},
-		{"s", type_string},
-		{NULL, NULL}
-	};
-
-	while (type->letter[i] != NULL)
-	{
-		if (strcmp(type->letter[i], s) == 0)
-		{
-			return(type->f);
-		}
-	}
-}*/
-
+/**
+ * print_all - function that print undefined amount
+ * of args, no matter the type it got
+ * @format: string of char
+ * eache letter should be the type of and undefined
+ * arg.
+ *
+ * Return:
+ */
 void print_all(const char * const format, ...)
 {
-	struct type type[] =
-	{
+	/**
+	* struct type - structure that list
+	* type of arg and pointer to the function
+	* we need to use to print it
+	 *
+	* @letter: Letter code standing for a data type
+	* @f: Function that print args of the data type given by letter
+	*/
+	struct type type[] = {
 		{"c", type_char},
 		{"i", type_int},
 		{"f", type_float},
@@ -57,16 +83,16 @@ void print_all(const char * const format, ...)
 	};
 
 	int k = 0;
-	char *separator ="";
- 
+	char *separator = "";
+
 	va_list args;
 
 	va_start(args, format);
 
-while (format != NULL && format[k] != '\0')
+	while (format != NULL && format[k] != '\0')
 	{
 		int j = 0;
-		
+
 		while (type[j].letter != NULL)
 		{
 			if (format[k] == type[j].letter[0])
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -8,16 +8,24 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void (*get_data_type(char *s)); 
+void (*get_data_type(char *s));
 void type_char (va_list args);
 void type_int (va_list args);
 void type_float (va_list args);
-void type_string (va_list args);
+void type_string(va_list args);
 
+/**
+ * struct type - structure that list
+ * type of arg and pointer to the function
+ * we need to use to print it
+ *
+ * @letter: Letter code standing for a data type
+ * @f: Function that print args of the data type given by letter
+ */
 struct type
 {
-	char* letter;
-	void (*f) (va_list);
+	char *letter;
+	void (*f)(va_list);
 };
 
 #endif
