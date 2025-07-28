bf2c9658c030e6412dc6bff0141b808a3f48b723
Mathieu <mathieu.mjr@gmail.com>
2025-07-28 13:40:49 +0200

    fixed betty style

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,17 +4,17 @@
  * print_list - print every element of a list
  * @h: is a pointer to 1st element of a list
  *
- * Return: number of element printed
+ * Return: the number of element printed
  */
 size_t print_list(const list_t *h)
 {
 	int i;
-	
+
 	for (i = 0; h->next != NULL; ++i)
 	{
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 	}
 	printf("[%d] %s\n", h->len, h->str);
-	return(i + 1);
+	return (i + 1);
 }
