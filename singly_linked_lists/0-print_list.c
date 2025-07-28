a92d2d568b379612819629010796b21185a933cf
Mathieu <mathieu.mjr@gmail.com>
2025-07-28 13:34:05 +0200

    function that print all element of a list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,6 +1,20 @@
 #include "lists.h"
 
+/**
+ * print_list - print every element of a list
+ * @h: is a pointer to 1st element of a list
+ *
+ * Return: number of element printed
+ */
 size_t print_list(const list_t *h)
 {
+	int i;
 	
+	for (i = 0; h->next != NULL; ++i)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	printf("[%d] %s\n", h->len, h->str);
+	return(i + 1);
 }
