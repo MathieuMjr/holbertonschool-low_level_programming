017cbbc61b90a7c0919b6873fee571dcf8531b19
Mathieu <mathieu.mjr@gmail.com>
2025-07-29 12:16:50 +0200

    fixed documentation

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -4,7 +4,7 @@
  * print_dlistint - read each element of a doubly linked list
  * @h: adress of the 1st element of the list
  *
- * Return: a pointer to the newly created node
+ * Return: number of element in the list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * dlistint_len - return the number of element of the list
+ * dlistint_len - return the number of element of a boudly
+ * linked list
  * @h: adress of the 1st element of the list
  *
  * Return: number of element in the list
