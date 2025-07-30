63a9e07946f45bfa968668ee9dc1eef93d5b3451
Mathieu <mathieu.mjr@gmail.com>
2025-07-30 16:45:20 +0200

    betty style fixed

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (*h == NULL)
 		return (add_dnodeint(h, n));
-	
+
 	if (idx == 0)
 	{
 		new_node = add_dnodeint(h, n);
@@ -34,11 +34,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp = temp->next;
 		index++;
 	}
-	if (temp == NULL ||temp->next == NULL)
+	if (temp == NULL || temp->next == NULL)
 		return (NULL);
 	node_prev = temp;
 	node_next = temp->next;
-	
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
