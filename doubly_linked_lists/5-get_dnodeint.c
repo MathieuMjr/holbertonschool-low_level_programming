2e76b341bf16b7af36b16605b12d53740f4ff51c
Mathieu <mathieu.mjr@gmail.com>
2025-07-30 10:35:18 +0200

    Fixed case where index is to hight

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -14,13 +14,14 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *h = head;
 
 	if (index == 0)
-	{
 		return (head);
-	}
 	while (i < index)
 	{
 		h = h->next;
+		if (h == NULL)
+			return (NULL);
 		i++;
 	}
+
 	return (h);
 }
