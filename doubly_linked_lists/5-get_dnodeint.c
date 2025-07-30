c645d51850cfe094030c8fe1e78ff1f2d339bc1a
Mathieu <mathieu.mjr@gmail.com>
2025-07-30 09:54:07 +0200

    function that return adress of the node at the wanted index

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - get the node at index
+ * @head: adress of the 1st node of the list
+ * @index: position of the node in the list
+ * you want the adress
+ *
+ * Return: adress of the node at the wanted index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *h = head;
+
+	if (index == 0)
+	{
+		return(head);
+	}
+	while(i < index)
+	{
+		h = h->next;
+		i++;
+	}
+	return(h);
+}
