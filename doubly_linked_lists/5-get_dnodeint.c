a8fe0bb8c5c30ac958120485058e7fa0ddee86f2
Mathieu <mathieu.mjr@gmail.com>
2025-07-30 09:56:04 +0200

    fixed betty

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -15,12 +15,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	if (index == 0)
 	{
-		return(head);
+		return (head);
 	}
-	while(i < index)
+	while (i < index)
 	{
 		h = h->next;
 		i++;
 	}
-	return(h);
+	return (h);
 }
