300f4bdabd6ae69a7ac6ab38c19101e045cc208b
Mathieu <mathieu.mjr@gmail.com>
2025-07-30 17:39:35 +0200

    Commented

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,18 +12,22 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *h = *head;
+	/*je copie l'adresse de la tête dans un pointeur*/
 	dlistint_t *new = malloc(sizeof(dlistint_t));
 
-	if (new == NULL)
+	if (new == NULL) /*malloc a réussi ? si non, retourne NULL*/
 		return (NULL);
 
 	if (*head != NULL)
 	{
 		h->prev = new;
 	}
+	/*si une liste existe, on va insérer avant la tête
+	Il faut modifier le champs "previous" de la tête donnée
+	au début pour qu'elle pointe vers notre nouveau noeud*/
 	new->n = n;
-	new->prev = NULL;
-	new->next = *head;
-	*head = new;
+	new->prev = NULL; /*notre nouveau noeud devient le 1er de la liste*/
+	new->next = *head;/*l'élément suivant était le 1er au début, donc la head*/
+	*head = new;/*la nouvelle head est mise sur notre nouveau (1er) noeud*/
 	return (*head);
 }
--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -10,11 +10,17 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	unsigned int i = 0;
+	unsigned int i = 0; 
+	/*ce compteur sert à limiter les tours de boucles*/
 	dlistint_t *h = head;
+	/*ce pointeur permet de se déplacer dans la liste
+	sans modifier le pointeur de tête à la fin de la fonction*/
 
 	if (index == 0)
 		return (head);
+	/*si on veut l'index 0, me 1er element
+	on donne la tête directement - la boucle while
+	ne démarre pas si index == 0*/
 	while (i < index)
 	{
 		h = h->next;
@@ -22,6 +28,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 			return (NULL);
 		i++;
 	}
+	/*si jamais h est NULL, c'est qu'on a dépassé les limites
+	de la liste !*/
+	/*i < index car sinon, h va essayer de se déplacer sur
+	le noeud suivant.*/
 
 	return (h);
 }
