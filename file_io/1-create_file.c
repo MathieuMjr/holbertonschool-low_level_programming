d0f7117ea5f58e5346168cdef00480419d4e72e5
Mathieu <mathieu.mjr@gmail.com>
2025-07-31 16:26:42 +0200

    function that create a new file and write in it

---
+++ file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile - read and print a text file on
+ * stdout
+ * @filename: name of the file to open
+ * @letters: number of byte to read and write
+ *
+ * Return: number of byte written
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int i = 0;
+	ssize_t written;
+	
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+
+	if (text_content == NULL)
+		text_content = "";
+	else
+	{
+		while(text_content[i] != '\0')
+		{
+			++i;
+		}
+	}
+	written = write(fd, text_content, i + 1);
+	if (written != (i + 1))
+		return (-1);
+	return (1);
+}
