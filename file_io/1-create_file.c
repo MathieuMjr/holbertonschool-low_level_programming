9a195d62a51e9d80102c2b928230440c8fbbb47c
Mathieu <mathieu.mjr@gmail.com>
2025-07-31 17:04:41 +0200

    fixed close requierement and check if open succeed

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,6 +20,9 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
 	/*si existe déjà, ouvrir en write only et efface (trunc). Sinon, create (creat)*/
 	/*si le fichier est créé, mettre les autorisations lecture et écriture au owner*/
+	if (fd == -1)
+		close (fd);
+		return (-1);
 	if (text_content == NULL) /*si pas de texte, imprimer rien*/
 		text_content = "";
 	else
@@ -31,6 +34,9 @@ int create_file(const char *filename, char *text_content)
 	}
 	written = write(fd, text_content, i);/*écrit sur la sortie fd (le fichier ouvert) le text_content dont la taille est i bytes*/
 	if (written != (i))/*si pas de bug, written retourne ce qui a été écrit, donc longueur de i. Si bug, retourner -1*/
+	{
+		close (fd);
 		return (-1);
+	}
 	return (1);
 }
