40955c41192741c62611b7fbcd3e3ce4b588a2c8
Mathieu <mathieu.mjr@gmail.com>
2025-07-31 16:28:41 +0200

    updated lenght written

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -28,8 +28,8 @@ int create_file(const char *filename, char *text_content)
 			++i;
 		}
 	}
-	written = write(fd, text_content, i + 1);
-	if (written != (i + 1))
+	written = write(fd, text_content, i);
+	if (written != (i))
 		return (-1);
 	return (1);
 }
