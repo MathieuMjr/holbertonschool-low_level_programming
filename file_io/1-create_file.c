c3a2050800bd492bbd2e420cc6aa8fc598118e93
Mathieu <mathieu.mjr@gmail.com>
2025-07-31 16:34:17 +0200

    commented

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,22 +14,23 @@ int create_file(const char *filename, char *text_content)
 	int i = 0;
 	ssize_t written;
 
-	if (filename == NULL)
+	if (filename == NULL) /*retourne une erreur si pas de nom de fichier donné*/
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
-
-	if (text_content == NULL)
+	/*si existe déjà, ouvrir en write only et efface (trunc). Sinon, create (creat)*/
+	/*si le fichier est créé, mettre les autorisations lecture et écriture au owner*/
+	if (text_content == NULL) /*si pas de texte, imprimer rien*/
 		text_content = "";
 	else
 	{
-		while (text_content[i] != '\0')
-		{
+		while (text_content[i] != '\0')/*calculer la taille de la chaine à écrire*/
+		{								/*servira pour le nombre de byte a donner à write*/
 			++i;
 		}
 	}
-	written = write(fd, text_content, i);
-	if (written != (i))
+	written = write(fd, text_content, i);/*écrit sur la sortie fd (le fichier ouvert) le text_content dont la taille est i bytes*/
+	if (written != (i))/*si pas de bug, written retourne ce qui a été écrit, donc longueur de i. Si bug, retourner -1*/
 		return (-1);
 	return (1);
 }
