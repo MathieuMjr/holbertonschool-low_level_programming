b9ea983fb0d0e8fb0c99f0755124f6ea70b20561
Mathieu <mathieu.mjr@gmail.com>
2025-08-01 16:48:07 +0200

    function that create a node and store its adress in a hashtable

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_et - create a new node and store
+ * it's adresse in hashtable
+ * @ht: adress of a hashtable
+ * @key: key of the values
+ * @value: value of a node
+ *
+ * Return: 1 if succeed et 0 if fails
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node;
+	unsigned long int index;
+	hash_node_t *temp;
+
+	if (key == NULL)
+		return (0);
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+
+	index = key_index((const unsigned char*)key, ht->size);
+	if (ht->array[index] == NULL)
+		ht->array[index] = new_node;
+	else
+	{
+		temp = ht->array[index];
+		ht->array[index] = new_node;
+		new_node->next = temp;
+	}
+
+	return(0);
+}
