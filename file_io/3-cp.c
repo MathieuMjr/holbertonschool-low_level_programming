c1db9bf6cc75999fc76e0d39b19f10696061eac6
Mathieu <mathieu.mjr@gmail.com>
2025-08-01 10:05:43 +0200

    function that copy content of a file in another one

---
+++ file_io/3-cp.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+ * cp_file - copy file content into another one
+ * @src_filename: path of the source file
+ * @dest_filename: path of the destination file
+ *
+ * Return: 1 if succeed
+ */
+int cp_file(const char *src_filename, const char *dest_filename)
+{
+	int src_fd;
+	int dest_fd;
+	ssize_t been_read;
+	ssize_t written;
+	void *buffer;
+
+	src_fd = open(src_filename, O_RDONLY);
+	if (src_fd == -1)
+	{
+		dprintf(2, "Error: Can't read from fil %s\n", src_filename);
+		exit(98);
+	}
+	dest_fd = open(dest_filename, O_CREAT | O_WRONLY | O_TRUNC,
+		S_IWUSR | S_IRUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (dest_fd == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", dest_filename);
+		exit(99);
+	}
+	buffer = malloc(1024);
+	if (buffer == NULL)
+		return (-1);
+	while ((been_read = read(src_fd, buffer, 1024)) > 0)
+	{
+		if (been_read == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", src_filename);
+			free(buffer);
+			exit(98);
+		}
+		written = write(dest_fd, buffer, been_read);
+		if (written == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", dest_filename);
+			free(buffer);
+			exit(99);
+		}
+	}
+	free(buffer);
+	close(src_fd);
+	close(dest_fd);
+	if (src_fd == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", src_fd);
+		exit(100);
+	}
+	if (dest_fd == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", dest_fd);
+		exit(100);
+	}
+	return (1);
+}
