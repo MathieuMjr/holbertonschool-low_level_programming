34cc5873046494d8f7aa2e37bdd1604955253eeb
Mathieu <mathieu.mjr@gmail.com>
2025-08-03 19:17:56 +0200

    fixed printing ", ", coding style and description

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * ash_table_print - print all data in
+ * hash_table_print - print all data in
  * the hash table
  * @ht: pointer to a hashtable
  *
@@ -11,26 +11,22 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int array_index = 0;
 	hash_node_t *reading_head;
-	int loop_counter = 0;
+	int print_counter = 0;
 
-	
-	
 	if (ht == NULL)
 		printf("{}");
-	
+
 	printf("{");
-	while (array_index <= ht->size)
+	while (array_index < ht->size)
 	{
 		reading_head = ht->array[array_index];
-		while(reading_head != NULL)
-		{	
-			if (array_index > 0 && loop_counter > 0)
+		while (reading_head != NULL)
+		{
+			if (print_counter > 0)
 				printf(", ");
 			printf("'%s': '%s'", reading_head->key, reading_head->value);
-			if (reading_head->next != NULL)
-				printf(", ");
+			print_counter++;
 			reading_head = reading_head->next;
-			loop_counter++;
 		}
 		array_index++;
 	}
