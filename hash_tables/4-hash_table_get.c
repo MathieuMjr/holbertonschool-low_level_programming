182c4d47007ae6fba512ed98b36302ccbb8041d1
Mathieu <mathieu.mjr@gmail.com>
2025-08-03 16:01:32 +0200

    function that get a value from the key in a hashtable

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - find a value associated
+ * to a specific key in the hashtable
+ * @ht: pointer to a hashtable
+ * @key: key of a node
+ *
+ * Return: value of the element of NULL if
+ * key not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node_pointer;
+	
+	if (ht == NULL || key == NULL)
+		return (NULL);
+	
+	index = key_index((const unsigned char *)key, ht->size);
+	
+	node_pointer = ht->array[index];
+
+	while (node_pointer != NULL && strcmp(node_pointer->key, key) != 0)
+	{
+		node_pointer = node_pointer->next;
+	}
+	if (node_pointer == NULL)
+		return (NULL);
+	return (node_pointer->value);
+}
